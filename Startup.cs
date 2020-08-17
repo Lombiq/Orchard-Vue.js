2849ecd9f4562a425b5f2d041c99552ff85792c2
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2020-08-17 22:41:19 +0200

    Code cleanup

--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,25 @@
 using Lombiq.VueJs.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.DisplayManagement.Descriptors.ShapeTemplateStrategy;
+using OrchardCore.Modules;
 using OrchardCore.ResourceManagement;
+using System;
 
 namespace Lombiq.VueJs
 {
-    public class Startup
+    public class Startup : StartupBase
     {
-        public void ConfigureServices(IServiceCollection services)
+        public override void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<IShapeTemplateHarvester, VueComponentTemplateHarvester>();
             services.AddScoped<IResourceManifestProvider, ResourceManifest>();
+        }
+
+        public override void Configure(IApplicationBuilder app, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
+        {
+            // No need for anything here yet.
         }
     }
 }
