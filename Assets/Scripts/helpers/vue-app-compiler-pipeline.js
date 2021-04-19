526f86826499d4b2aaa463d85d8afd0951b131bd
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2021-04-20 00:21:10 +0200

    Migrating to NPM Targets and Gulp Extensions, code styling

--- Assets/Scripts/helpers/vue-app-compiler-pipeline.js
+++ Assets/Scripts/helpers/vue-app-compiler-pipeline.js
@@ -16,17 +16,17 @@ const getVueApps = require('./get-vue-apps');
 
 const defaultOptions = {
     rootPath: './Assets/Apps/',
-    destinationPath: './wwwroot/Apps/',
+    destinationPath: './wwwroot/apps/',
     vueJsNodeModulesPath: '../Lombiq.VueJs/node_modules',
     rollupAlias: {},
-    isProduction: false
+    isProduction: false,
 };
 
-const getVueAppCompilerPipeline = options => {
+function compile(options) {
     const opts = options ? { ...defaultOptions, ...options } : defaultOptions;
 
     return all(getVueApps(opts.rootPath)
-        .map(function (appName) {
+        .map((appName) => {
             const entryPath = path.join(opts.rootPath, appName, '/main.js');
 
             return gulp.src(entryPath)
@@ -35,7 +35,7 @@ const getVueAppCompilerPipeline = options => {
                     allowRealFiles: true,
                     input: path.join(entryPath),
                     output: {
-                        format: 'cjs'
+                        format: 'cjs',
                     },
                     onwarn: (warning, next) => {
                         if (warning.code === 'THIS_IS_UNDEFINED') return;
@@ -43,23 +43,26 @@ const getVueAppCompilerPipeline = options => {
                     },
                     plugins: [
                         json(),
-                        nodeResolve({ preferBuiltins: true, browser: true, mainFields: [ 'module', 'jsnext:main' ] }),
+                        nodeResolve({ preferBuiltins: true, browser: true, mainFields: ['module', 'jsnext:main'] }),
                         alias({
-                            vue: path.resolve(path.join(opts.vueJsNodeModulesPath, opts.isProduction ? 'vue/dist/vue.common.prod.js' : 'vue/dist/vue.esm.browser.js')),
+                            vue: path.resolve(path.join(opts.vueJsNodeModulesPath, opts.isProduction
+                                ? 'vue/dist/vue.common.prod.js'
+                                : 'vue/dist/vue.esm.browser.js')),
                             vuelidate: path.resolve(path.join(opts.vueJsNodeModulesPath, 'vuelidate/')),
-                            'vue-router': path.resolve(path.join(opts.vueJsNodeModulesPath, 'vue-router/dist/vue-router.common.js')),
+                            'vue-router': path.resolve(path.join(
+                                opts.vueJsNodeModulesPath, 'vue-router/dist/vue-router.common.js')),
                             'vue-axios': path.resolve(path.join(opts.vueJsNodeModulesPath, 'vue-axios/')),
                             axios: path.resolve(path.join(opts.vueJsNodeModulesPath, 'axios/')),
                             resolve: ['.js', '/index.js', '/lib/index.js', '/src/index.js'],
-                            ...opts.rollupAlias
+                            ...opts.rollupAlias,
                         }),
                         replace({
                             'process.env.NODE_ENV': JSON.stringify(opts.isProduction ? 'production' : 'development'),
-                            'process.env.BUILD': JSON.stringify('web')
+                            'process.env.BUILD': JSON.stringify('web'),
                         }),
                         commonjs(),
-                        buble()
-                    ]
+                        buble(),
+                    ],
                 }))
                 .pipe(rename(appName + '.js'))
                 .pipe(gulp.dest(opts.destinationPath))
@@ -67,6 +70,6 @@ const getVueAppCompilerPipeline = options => {
                 .pipe(rename(appName + '.min.js'))
                 .pipe(gulp.dest(opts.destinationPath));
         }));
-};
+}
 
-module.exports = getVueAppCompilerPipeline;
+module.exports = { compile };
