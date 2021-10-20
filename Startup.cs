f425ffd41bcae8a3a3a48ad8ccbfd270534dc231
Dávid El-Saig <david.el-saig@lombiq.com>
2021-10-20 19:14:09 +0200

    Downgrade for rc2-13929.

--- Startup.cs
+++ Startup.cs
@@ -2,7 +2,6 @@ using Lombiq.VueJs.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 using OrchardCore.DisplayManagement.Descriptors.ShapeTemplateStrategy;
 using OrchardCore.Modules;
 using OrchardCore.ResourceManagement;
@@ -15,7 +14,7 @@ namespace Lombiq.VueJs
         public override void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<IShapeTemplateHarvester, VueComponentTemplateHarvester>();
-            services.AddTransient<IConfigureOptions<ResourceManagementOptions>, ResourceManagementOptionsConfiguration>();
+            services.AddScoped<IResourceManifestProvider, ResourceManifest>();
         }
 
         public override void Configure(IApplicationBuilder app, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
