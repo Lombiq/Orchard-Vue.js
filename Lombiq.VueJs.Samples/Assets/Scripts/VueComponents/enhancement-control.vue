725c8460c338832c63d475b65330dfa1e1fdf941
Dávid El-Saig <david.el-saig@lombiq.com>
2022-02-08 22:17:48 +0100

    Add SFCs.

---
+++ Lombiq.VueJs.Samples/Assets/Scripts/VueComponents/enhancement-control.vue
@@ -0,0 +1,38 @@
+﻿<template>
+    <div class="d-flex justify-content-between">
+        <button type="button"
+                class="btn btn-primary"
+                :class="{ disabled: value <= 1 }"
+                @click.prevent="$emit('input', value - 1)">
+            [[ << ]]
+        </button>
+        <button v-for="page in max"
+                :key="'pager-' + page"
+                class="btn btn-primary"
+                :class="{ disabled: page === value }"
+                @click.prevent="$emit('input', page)">
+            {{ page }}
+        </button>
+        <button type="button"
+                class="btn btn-primary"
+                :class="{ disabled: value >= max }"
+                @click.prevent="$emit('input', value + 1)">
+            [[ >> ]]
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        value: {
+            type: Number,
+            required: true
+        },
+        max: {
+            type: Number,
+            required: true
+        }
+    },
+}
+</script>
---
+++ Lombiq.VueJs.Samples/Assets/Scripts/VueComponents/enhancement-table-body.vue
@@ -0,0 +1,36 @@
+﻿<template>
+    <tbody>
+        <tr v-for="row in rows" :key="row.id">
+            <td>{{ row.number }}</td>
+            <td>{{ row.date }}</td>
+            <td>{{ row.day }}</td>
+            <td>{{ row.stars }}</td>
+        </tr>
+    </tbody>
+</template>
+
+<script>
+const ten = Array.from({ length: 10 });
+
+export default {
+    props: {
+        data: {
+            type: Array,
+            required: true,
+        }
+    },
+    computed: {
+        rows(self) {
+            return self.data.map((row, rowIndex) => Object.assign(
+                { },
+                row,
+                {
+                    id: 'enhancement-table-body-row-' + rowIndex,
+                    stars: ten
+                        .map((_, index) => index < row.random ? '★' : '✰')
+                        .join(''),
+                }));
+        },
+    },
+};
+</script>
