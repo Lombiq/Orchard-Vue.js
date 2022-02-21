f20a95fe094845371603cc65da0300d7a339a5ed
Dávid El-Saig <david.el-saig@lombiq.com>
2022-02-21 08:29:02 +0100

    Add compiled asset files because nuget.

---
+++ Lombiq.VueJs.Samples/wwwroot/vue/enhancement-control.js
@@ -0,0 +1,14 @@
+'use strict';
+
+window.Vue.component('enhancement-control', { name: 'enhancement-control', template: document.querySelector('.VueComponent-EnhancementControl').innerHTML, /* eslint-disable-line */ /* eslint-disable linebreak-style */
+    props: {
+        value: {
+            type: Number,
+            required: true,
+        },
+        max: {
+            type: Number,
+            required: true,
+        },
+    },
+});
