f106c1f33d04dcaf66848f88cbb7129c6d330380
Dávid El-Saig <david.el-saig@lombiq.com>
2022-03-24 15:36:21 +0100

    Add resource name constants.

--- Lombiq.VueJs/Constants/ResourceNames.cs
+++ Lombiq.VueJs/Constants/ResourceNames.cs
@@ -4,4 +4,10 @@ public static class ResourceNames
 {
     public const string Vue = "vuejs";
     public const string Es6PromisePolyfill = nameof(Es6PromisePolyfill);
+
+    public static class Components
+    {
+        public const string ContentItemDisplay = "content-item-display";
+        public const string DateTime = "date-time";
+    }
 }
