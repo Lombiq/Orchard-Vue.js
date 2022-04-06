4d3d639d7daf464166ad0e4afdda6c675c32e176
Dávid El-Saig <david.el-saig@lombiq.com>
2022-04-06 03:19:19 +0200

    Add bootstrap-split-button component.

---
+++ Lombiq.VueJs/Assets/Scripts/VueComponents/bootstrap-split-button.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="btn-group">
+        <button type="button" :class="buttonClass">Action</button>
+        <button type="button" class="dropdown-toggle dropdown-toggle-split" :class="buttonClass" data-bs-toggle="dropdown" aria-expanded="false">
+            <span class="visually-hidden">[[ Toggle Dropdown ]]</span>
+        </button>
+        <ul class="dropdown-menu">
+            <li v-for="option in options" :key="option.text">
+                <a v-if="option.text && option.text.trim && option.text.trim().startsWith('---')">
+                    <hr class="dropdown-divider">
+                </a>
+                <a v-else :href="option.href" class="dropdown-item">
+                    {{ option.text }}
+                </a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        type: { type: String, default: 'primary' },
+        text: { default: null, },
+        options: { type: Array, required: true }, // { text: String, href: String }[]
+    },
+    computed: {
+        buttonClass(self) { return 'btn btn-' + self.type; },
+    }
+}
+</script>
