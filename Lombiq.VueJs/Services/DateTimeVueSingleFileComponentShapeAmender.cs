f79dae351e19a642ff19487073115b0dfb711eef
Roland Márkus <roland.markus@lombiq.com>
2022-09-30 17:24:15 +0200

    Adding to Vue.

--- Lombiq.VueJs/Services/DateTimeVueSingleFileComponentShapeAmender.cs
+++ Lombiq.VueJs/Services/DateTimeVueSingleFileComponentShapeAmender.cs
@@ -1,3 +1,4 @@
+using OrchardCore.Localization;
 using OrchardCore.Modules;
 using System.Globalization;
 using System.Threading.Tasks;
@@ -7,16 +8,21 @@ namespace Lombiq.VueJs.Services;
 public class DateTimeVueSingleFileComponentShapeAmender : ServerSideValuesVueSingleFileComponentShapeAmenderBase
 {
     private readonly ILocalClock _localClock;
+    private readonly ILocalizationService _localizationService;
 
     protected override string ShapeName => "VueComponent-DateTime";
     protected override string PropertyName => "dateTime";
 
-    public DateTimeVueSingleFileComponentShapeAmender(ILocalClock localClock) => _localClock = localClock;
+    public DateTimeVueSingleFileComponentShapeAmender(ILocalClock localClock, ILocalizationService localizationService)
+    {
+        _localClock = localClock;
+        _localizationService = localizationService;
+    }
 
     protected override async ValueTask<object> GetPropertyValueAsync(string shapeName) =>
         new
         {
             TimeZone = (await _localClock.GetLocalTimeZoneAsync()).TimeZoneId,
-            Culture = CultureInfo.CurrentCulture.Name, // Same as "@Orchard.CultureName()".
+            Culture = await _localizationService.GetDefaultCultureAsync(), // Same as "@Orchard.CultureName()".
         };
 }
