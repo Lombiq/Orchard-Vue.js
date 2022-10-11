c308ba536d5a502bd9e1abd39727e2c2c83c913d
Dávid El-Saig <david.el-saig@lombiq.com>
2022-10-11 14:23:58 +0200

    props and targets

--- Lombiq.VueJs/Manifest.cs
+++ Lombiq.VueJs/Manifest.cs
@@ -4,7 +4,7 @@ using OrchardCore.Modules.Manifest;
     Name = "Lombiq Vue.js",
     Author = "Lombiq Technologies",
     Website = "https://github.com/Lombiq/Orchard-Vue.js",
-    Version = "0.0.1",
+    Version = "0.0.2",
     Description = "Adds Vue.js to Orchard Core.",
     Category = "Vue.js"
 )]
