6fb5c926805930ffe4e6682c824d3305c8a7aa9b
Roland Márkus <roland.markus@lombiq.com>
2022-10-13 18:36:03 +0200

    Fix.

--- Lombiq.VueJs/Services/DateTimeVueSingleFileComponentShapeAmender.cs
+++ Lombiq.VueJs/Services/DateTimeVueSingleFileComponentShapeAmender.cs
@@ -1,5 +1,5 @@
+using OrchardCore.DisplayManagement.Razor;
 using OrchardCore.Modules;
-using System.Globalization;
 using System.Threading.Tasks;
 
 namespace Lombiq.VueJs.Services;
@@ -7,17 +7,21 @@ namespace Lombiq.VueJs.Services;
 public class DateTimeVueSingleFileComponentShapeAmender : ServerSideValuesVueSingleFileComponentShapeAmenderBase
 {
     private readonly ILocalClock _localClock;
+    private readonly IOrchardDisplayHelper _displayHelper;
 
     protected override string ShapeName => "VueComponent-DateTime";
     protected override string PropertyName => "dateTime";
 
-    public DateTimeVueSingleFileComponentShapeAmender(ILocalClock localClock) =>
+    public DateTimeVueSingleFileComponentShapeAmender(ILocalClock localClock, IOrchardDisplayHelper displayHelper)
+    {
         _localClock = localClock;
+        _displayHelper = displayHelper;
+    }
 
     protected override async ValueTask<object> GetPropertyValueAsync(string shapeName) =>
         new
         {
             TimeZone = (await _localClock.GetLocalTimeZoneAsync()).TimeZoneId,
-            Culture = CultureInfo.CurrentUICulture.IetfLanguageTag, // Same as "@Orchard.CultureName()".
+            Culture = _displayHelper.CultureName(), // Same as "@Orchard.CultureName()".
         };
 }
