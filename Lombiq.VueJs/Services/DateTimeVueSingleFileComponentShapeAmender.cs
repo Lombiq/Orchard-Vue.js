4baebb1e88616f73ac4342b975f2cbb9f0c68d24
Roland Márkus <roland.markus@lombiq.com>
2022-10-13 17:49:13 +0200

    Using IetfLanguageTag.

--- Lombiq.VueJs/Services/DateTimeVueSingleFileComponentShapeAmender.cs
+++ Lombiq.VueJs/Services/DateTimeVueSingleFileComponentShapeAmender.cs
@@ -1,5 +1,5 @@
-using OrchardCore.Localization;
 using OrchardCore.Modules;
+using System.Globalization;
 using System.Threading.Tasks;
 
 namespace Lombiq.VueJs.Services;
@@ -7,21 +7,17 @@ namespace Lombiq.VueJs.Services;
 public class DateTimeVueSingleFileComponentShapeAmender : ServerSideValuesVueSingleFileComponentShapeAmenderBase
 {
     private readonly ILocalClock _localClock;
-    private readonly ILocalizationService _localizationService;
 
     protected override string ShapeName => "VueComponent-DateTime";
     protected override string PropertyName => "dateTime";
 
-    public DateTimeVueSingleFileComponentShapeAmender(ILocalClock localClock, ILocalizationService localizationService)
-    {
+    public DateTimeVueSingleFileComponentShapeAmender(ILocalClock localClock) =>
         _localClock = localClock;
-        _localizationService = localizationService;
-    }
 
     protected override async ValueTask<object> GetPropertyValueAsync(string shapeName) =>
         new
         {
             TimeZone = (await _localClock.GetLocalTimeZoneAsync()).TimeZoneId,
-            Culture = await _localizationService.GetDefaultCultureAsync(), // Same as "@Orchard.CultureName()".
+            Culture = CultureInfo.CurrentUICulture.IetfLanguageTag, // Same as "@Orchard.CultureName()".
         };
 }
