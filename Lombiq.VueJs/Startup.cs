703019582ee72e9d75d76d51d80b5a10d1ad6d52
Oliver Friedrich <oliver.friedrich@lombiq.com>
2022-11-25 13:14:54 +0100

    Revert "Revert "Merge pull request #59 from Lombiq/issue/OSOE-92""
    This reverts commit 277c4ba6a103c9c63f61f3184379c4804403b357.

--- Lombiq.VueJs/Startup.cs
+++ Lombiq.VueJs/Startup.cs
@@ -1,10 +1,8 @@
 using Lombiq.VueJs.Services;
 using Lombiq.VueJs.TagHelpers;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 using OrchardCore.DisplayManagement.Descriptors.ShapeTemplateStrategy;
 using OrchardCore.Modules;
-using OrchardCore.ResourceManagement;
 
 namespace Lombiq.VueJs;
 
@@ -13,7 +11,6 @@ public class Startup : StartupBase
     public override void ConfigureServices(IServiceCollection services)
     {
         services.AddScoped<IShapeTemplateHarvester, VueComponentTemplateHarvester>();
-        services.AddTransient<IConfigureOptions<ResourceManagementOptions>, ResourceManagementOptionsConfiguration>();
 
         services.AddScoped<IShapeTemplateHarvester, VueSingleFileComponentTemplateHarvester>();
         services.AddScoped<IShapeTemplateViewEngine, VueSingleFileComponentShapeTemplateViewEngine>();
