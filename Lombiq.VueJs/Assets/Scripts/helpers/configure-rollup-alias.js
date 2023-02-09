15d80e579b4cff694562ada49e29770240f39f13
Benedek Farkas <benedek.farkas@lombiq.com>
2023-02-09 20:40:47 +0100

    Adding spell-checking line ignore comment

--- Lombiq.VueJs/Assets/Scripts/helpers/configure-rollup-alias.js
+++ Lombiq.VueJs/Assets/Scripts/helpers/configure-rollup-alias.js
@@ -5,8 +5,8 @@ module.exports = function configureRollupAlias(vueJsNodeModulesPath, isProductio
     const entries = {
         vue: path.resolve(vueJsNodeModulesPath, isProduction
             ? 'vue/dist/vue.common.prod.js'
-            : 'vue/dist/vue.esm.browser.js'),
-        vuelidate: path.resolve(path.join(vueJsNodeModulesPath, 'vuelidate/')),
+            : 'vue/dist/vue.esm.browser.js'), // #spell-check-ignore-line
+        vuelidate: path.resolve(path.join(vueJsNodeModulesPath, 'vuelidate/')), // #spell-check-ignore-line
         'vue-router': path.resolve(path.join(
             vueJsNodeModulesPath, 'vue-router/dist/vue-router.common.js')),
         'vue-axios': path.resolve(path.join(vueJsNodeModulesPath, 'vue-axios/')),
