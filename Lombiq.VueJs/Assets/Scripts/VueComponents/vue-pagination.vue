d90e94b9e7f6188d436d465951c807a3b45c3cc7
Dávid El-Saig <david.el-saig@lombiq.com>
2023-02-27 18:59:27 +0100

    Add vue-pagination.

---
+++ Lombiq.VueJs/Assets/Scripts/VueComponents/vue-pagination.vue
@@ -0,0 +1,57 @@
+<template>
+    <div v-if="pageCount > 0" class="vuePagination">
+        <div class="vuePagination__item vuePagination__item_first"
+             :class="{ vuePagination__item_disabled: page === 0 }"
+             @click="page > 0 && $emit('change', 0)">
+            <i class="fas fa-chevron-double-left"></i>
+        </div>
+        <div class="vuePagination__item vuePagination__item_prev"
+             :class="{ vuePagination__item_disabled: page <= 0 }"
+             @click="page > 0 && $emit('change', page - 1)">
+            <i class="fas fa-chevron-left"></i> [[ Previous ]]
+        </div>
+        <div v-for="index in indexes"
+             class="vuePagination__item vuePagination__item_disabled"
+             :class="{ vuePagination__item_current: index === page }"
+             @click="index !== page && $emit('change', index)">
+            {{ index }}
+        </div>
+        <div class="vuePagination__item vuePagination__item_next"
+             :class="{ vuePagination__item_disabled: page + 1 >= pageCount }"
+             @click="page + 1 < pageCount && $emit('change', page + 1)">
+            <i class="fas fa-chevron-right"></i>
+        </div>
+        <div v-if="pageCount !== Number.POSITIVE_INFINITY"
+             class="vuePagination__item vuePagination__item_last"
+             :class="{ vuePagination__item_disabled: page + 1 === pageCount }"
+             @click="page + 1 < pageCount && $emit('change', pageCount - 1)">
+            <i class="fas fa-chevron-double-right"></i>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    model: {
+        prop: 'page',
+        event: 'change',
+    },
+    props: {
+        page: {
+            type: Number,
+            required: true,
+        },
+        pageCount: {
+            type: Number,
+            default: Number.POSITIVE_INFINITY,
+        },
+    },
+    computed: {
+        indexes(self) {
+            return Array.from({ length: 5 })
+                .map((_, index) => index - 2 + self.page )
+                .filter((index) => index >= 0 && index < self.pageCount);
+        },
+    },
+};
+</script>
