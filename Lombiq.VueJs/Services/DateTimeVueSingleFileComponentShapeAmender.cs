6d815148f51309b7bc77ba5a09834c3813f9e86a
Dávid El-Saig <david.el-saig@lombiq.com>
2023-02-28 19:45:15 +0100

    Fix locale info for <date-time>

--- Lombiq.VueJs/Services/DateTimeVueSingleFileComponentShapeAmender.cs
+++ Lombiq.VueJs/Services/DateTimeVueSingleFileComponentShapeAmender.cs
@@ -1,4 +1,4 @@
-using OrchardCore;
+using OrchardCore.Localization;
 using OrchardCore.Modules;
 using System.Threading.Tasks;
 
@@ -7,21 +7,23 @@ namespace Lombiq.VueJs.Services;
 public class DateTimeVueSingleFileComponentShapeAmender : ServerSideValuesVueSingleFileComponentShapeAmenderBase
 {
     private readonly ILocalClock _localClock;
-    private readonly IOrchardHelper _orchardHelper;
+    private readonly ILocalizationService _localizationService;
 
     protected override string ShapeName => "VueComponent-DateTime";
     protected override string PropertyName => "dateTime";
 
-    public DateTimeVueSingleFileComponentShapeAmender(ILocalClock localClock, IOrchardHelper orchardHelper)
+    public DateTimeVueSingleFileComponentShapeAmender(
+        ILocalClock localClock,
+        ILocalizationService localizationService)
     {
         _localClock = localClock;
-        _orchardHelper = orchardHelper;
+        _localizationService = localizationService;
     }
 
     protected override async ValueTask<object> GetPropertyValueAsync(string shapeName) =>
         new
         {
             TimeZone = (await _localClock.GetLocalTimeZoneAsync()).TimeZoneId,
-            Culture = _orchardHelper.CultureName(), // Same as "@Orchard.CultureName()".
+            Culture = await _localizationService.GetDefaultCultureAsync(),
         };
 }
