ac7c72bedd720a6f37d699e5962848c0d7ee1e4d
Sára El-Saig <sara.el-saig@lombiq.com>
2024-02-01 08:13:10 +0100

    Register script resources.

--- Lombiq.VueJs/ResourceManagementOptionsConfiguration.cs
+++ Lombiq.VueJs/ResourceManagementOptionsConfiguration.cs
@@ -7,13 +7,27 @@ namespace Lombiq.VueJs;
 
 public class ResourceManagementOptionsConfiguration : IConfigureOptions<ResourceManagementOptions>
 {
-    private const string Root = "~/" + Area;
+    private const string Root = $"~/{Area}/";
+    private const string Js = Root + "js/";
+    private const string Vendors = Root + "vendors/";
+
+    private const string VueVersion = "3.4.15";
+    private const string VueCdnRoot = $"https://unpkg.com/vue@{VueVersion}/dist/";
 
     private static readonly ResourceManifest _manifest = new();
 
-    static ResourceManagementOptionsConfiguration() => _manifest
+    static ResourceManagementOptionsConfiguration()
+    {
+        _manifest
+            .DefineScript(Vue3)
+            .SetUrl(Vendors + "vue/vue.esm-browser.prod.js", Vendors + "vue/vue.esm-browser.js")
+            .SetCdn(VueCdnRoot + "vue.esm-browser.prod.js", VueCdnRoot + "vue.esm-browser.js")
+            .SetVersion(VueVersion);
+
+        _manifest
             .DefineScript(VueComponentApp)
-            .SetUrl(Root + "/vue-component-app.min.js", Root + "/vue-component-app.js");
+            .SetUrl(Js + "vue-component-app.min.js", Js + "/vue-component-app.js");
+    }
 
     public void Configure(ResourceManagementOptions options) => options.ResourceManifests.Add(_manifest);
 }
