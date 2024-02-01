3a2d2faa7ab5928b8f4143cc58910ff735b24042
Sára El-Saig <sara.el-saig@lombiq.com>
2024-02-01 06:37:19 +0100

    Remove unnecessary vue package imports.

--- Lombiq.VueJs/Assets/Scripts/helpers/configure-rollup-alias.js
+++ Lombiq.VueJs/Assets/Scripts/helpers/configure-rollup-alias.js
@@ -6,11 +6,6 @@ module.exports = function configureRollupAlias(vueJsNodeModulesPath, isProductio
         vue: path.resolve(vueJsNodeModulesPath, isProduction
             ? 'vue/dist/vue.common.prod.js'
             : 'vue/dist/vue.esm.browser.js'), // #spell-check-ignore-line
-        vuelidate: path.resolve(path.join(vueJsNodeModulesPath, 'vuelidate/')), // #spell-check-ignore-line
-        'vue-router': path.resolve(path.join(
-            vueJsNodeModulesPath, 'vue-router/dist/vue-router.common.js')),
-        'vue-axios': path.resolve(path.join(vueJsNodeModulesPath, 'vue-axios/')),
-        axios: path.resolve(path.join(vueJsNodeModulesPath, 'axios/')),
         ...furtherRollupAliases,
     };
 
