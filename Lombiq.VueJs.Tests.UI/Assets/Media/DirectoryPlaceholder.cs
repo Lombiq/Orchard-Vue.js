cf46617f138b1d6559a68eb5bcfac8bb401f6e9a
Roland Márkus <roland.markus@lombiq.com>
2024-02-01 11:27:55 +0100

    Addressing warnings.

--- Lombiq.VueJs.Tests.UI/Assets/Media/DirectoryPlaceholder.cs
+++ Lombiq.VueJs.Tests.UI/Assets/Media/DirectoryPlaceholder.cs
@@ -1,6 +1,8 @@
 namespace Lombiq.VueJs.Tests.UI.Assets.Media;
 
 // This is a dummy class to easier access the requested resource.
+#pragma warning disable S2094 // Classes should not be empty
 internal sealed class DirectoryPlaceholder
+#pragma warning restore S2094 // Classes should not be empty
 {
 }
