b7395f0f36a369dc909b1a62046c02d3b47f8990
Sára El-Saig <sara.el-saig@lombiq.com>
2024-02-14 18:05:41 +0100

    Clean up and simplify vue app component.

--- Lombiq.VueJs/Assets/Scripts/vue-component-app.mjs
+++ Lombiq.VueJs/Assets/Scripts/vue-component-app.mjs
@@ -4,14 +4,12 @@ window.VueApplications = window.VueApplications ?? { };
 
 document.querySelectorAll('.lombiq-vue').forEach((element) => {
     const componentName = element.getAttribute('data-name');
-    const data = JSON.parse(element.getAttribute('data-model'));
+    const viewModel = JSON.parse(element.getAttribute('data-model'));
 
     if (!Array.isArray(window.VueApplications[componentName])) window.VueApplications[componentName] = [];
 
-    const app = createApp({
-        el: element,
-        data: data,
-    });
+    const app = createApp(import(componentName), viewModel);
     app.$appId = element.id;
     window.VueApplications[componentName].push(app);
+    app.mount(element);
 });
