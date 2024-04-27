378e5dc68b18675bb303ecf3bdeccdb25ea6ce58
Sára El-Saig <sara.el-saig@lombiq.com>
2024-04-27 15:17:03 +0200

    Add converter interface.

---
+++ Lombiq.VueJs/Services/IVueTemplateConverter.cs
@@ -0,0 +1,22 @@
+﻿using OrchardCore.DisplayManagement.Implementation;
+using System.Threading.Tasks;
+
+namespace Lombiq.VueJs.Services;
+
+/// <summary>
+/// A service that handles <c>[[{name} input ]]</c> expressions in Vue SFC templates. Used by <see
+/// cref="VueSingleFileComponentShapeTemplateViewEngine"/>.d
+/// </summary>
+public interface IVueTemplateConverter
+{
+    /// <summary>
+    /// Returns a value indicating whether this converter should handle the provided <paramref name="input"/>, typically
+    /// based on the <paramref name="name"/>.
+    /// </summary>
+    bool IsApplicable(string name, string input, DisplayContext displayContext);
+
+    /// <summary>
+    /// Returns the output that should be substituted instead of the provided expression.
+    /// </summary>
+    ValueTask<string> ConvertAsync(string name, string input, DisplayContext displayContext);
+}
